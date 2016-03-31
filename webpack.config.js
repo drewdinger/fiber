ce37574b8f393442917839d42f91ab5bdbab04d7
CallanFisher <cafi6580@colorado.edu>
2016-03-31 17:22:47 -0600

    webpack comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+// It requires both a path and webpack 
 var path = require('path');
 var webpack = require('webpack');
 
+// The webpack is deployed at localhost:3000 
 module.exports = {
   devtool: 'eval',
   entry: {
@@ -9,45 +11,64 @@ module.exports = {
       'webpack/hot/only-dev-server',
       './lib/index.js'],
   },
+  // Will output to the directory public/js/ into the file app.js
   output: {
     path: path.join(__dirname, './public/js/'),
     filename: `app.js`,
     publicPath: '/js/'
   },
+  // Replacing the pulgin (not sure about this)
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ],
+  // The node is empty in the file system
   node: {
     fs: "empty"
   },
+  // Add react to the path
   resolve: {
     alias: {
       'react': path.join(__dirname, 'node_modules', 'react')
     },
+    // add the extension .js
     extensions: ['', '.js']
   },
+  // add loaders
   resolveLoader: {
     'fallback': path.join(__dirname, 'node_modules')
   },
-  module: {    
+  // module class
+  module: {   
+  // Different types of loaders 
     loaders: [
     {
+      // test if the file ends in .js
       test: /\.js$/,
+      // check if the loader is react-hot or babel
       loaders: ['react-hot', 'babel'],
+      // don't add it to node_modules
       exclude: /node_modules/,
+      // include the dirname and it to the lib folder
       include: [path.join(__dirname,'./lib')]
     },
     {
+      // test if the file ends in .xml
       test: /\.xml$/,
+      // check if the loader is a raw
       loader: "raw"
     },
     {
+      // test if the file ends in .json
       test: /\.json$/,
+      // check if the loader is a json-loader
       loaders: ['json-loader']
     },
     {
+      // test if the file ends in .css
       test: /\.css?$/,
+      // check if the loader is style or raw
       loaders: ['style', 'raw'],
+      // include the directory name
       include: __dirname
     }]
   }
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,56 +1,78 @@
+// It requires both a path and webpack 
 var path = require('path');
 var webpack = require('webpack');
 
+// export the module to lib/index.js
 module.exports = {
   entry: {
     app : [
       './lib/index.js'],
   },
+  // output app.js to the directory name /public/js/ folcer
   output: {
     path: path.join(__dirname, './public/js/'),
     filename: `app.js`,
     publicPath: '/js/'
   },
+  // plugins for JSON.stringify
   plugins: [
     new webpack.DefinePlugin({
         'process.env': {
           'NODE_ENV': JSON.stringify('production')
         }
     }),
+    // Not sure what it means
     new webpack.optimize.UglifyJsPlugin({
     }),
   ],
+  // see if the file structure is empty
   node: {
     fs: "empty"
   },
+  // Add react to the path
   resolve: {
     alias: {
       'react': path.join(__dirname, 'node_modules', 'react')
     },
+    //ends in .js
     extensions: ['', '.js']
   },
+  // add loaders
   resolveLoader: {
     'fallback': path.join(__dirname, 'node_modules')
   },
+  // module class
   module: {
+    // Different types of loaders
     loaders: [
     {
+      // test if the file ends in .js
       test: /\.js$/,
+      // check if the loader is react-hot or babel
       loaders: ['react-hot', 'babel'],
+      // exclude it from the node_module folder
       exclude: /node_modules/,
+      // include the file to the directory name and add it the lib folder
       include: [path.join(__dirname,'./lib')]
     },
     {
+      // test if the file ends in .xml
       test: /\.xml$/,
+      // check if the loader is raw
       loader: "raw"
     },
     {
+      // test if the file ends in .json
       test: /\.json$/,
+      // check if the loader is json-loader
       loaders: ['json-loader']
     },
     {
+      // test if the file ends in .css
       test: /\.css?$/,
+      // check if the loader is style or raw
       loaders: ['style', 'raw'],
+      // the directory name
       include: __dirname
     }]
   }
